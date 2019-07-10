f0b355719aa0016b9cb87c9a2b6ec3f173240aa4
uguril <43095839+uguril@users.noreply.github.com>
2019-07-10 14:55:28 +0300

    GEKA

--- app/src/main/java/com/ugisoftware/htmlparse/MainActivity.java
+++ app/src/main/java/com/ugisoftware/htmlparse/MainActivity.java
@@ -521,6 +521,55 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         }
     }
+    private class VeriGetirGEKA extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            liste.clear();
+            linkliste.clear();
+            progressDialog= new ProgressDialog(MainActivity.this);
+            progressDialog.setTitle("Yükleniyor...");
+            progressDialog.setMessage("Lütfen bekleyiniz..");
+            progressDialog.setIndeterminate(false);
+            progressDialog.show();
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+
+
+            try {
+                Document doc= Jsoup.connect("http://geka.gov.tr/3073/guncel-destekler").timeout(30*1000).get();
+                for (Element adDiv : doc.select("ul.page-list")){
+
+                    Element duyuruDiv = adDiv.select("li").first();
+                    Element LinkDiv = duyuruDiv.select("a").first();
+                    liste.add("GEKA   : "+LinkDiv.text());
+
+                    linkliste.add(LinkDiv.absUrl("href")) ;
+                }
+                liste.add( "GEKA : Guncel Destekler" );
+                linkliste.add( "http://geka.gov.tr/3073/guncel-destekler" );
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+                liste.add("Connection Error");
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            lv.setAdapter( adapter );
+            progressDialog.dismiss();
+
+        }
+    }
 
     private class VeriGetirAnkara extends AsyncTask<Void, Void, Void> {
 
@@ -657,7 +706,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             new VeriGetirAhika().execute();
             //Aa
         }
-
+        if (id == R.id.action_gege) {
+            new VeriGetirGEKA().execute();
+            //Aa
+        }
         //menüden seçim yaptıktan sonra nav viewin kapalı konuma geçmesini sağlar
 
         drawer.closeDrawer(GravityCompat.START);
